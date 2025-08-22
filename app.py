5ebc8902b69c2b7dd0c39151d65924d966f7d14f
elzpdl09 <dkbaebeul@gmail.com>
2025-08-23 01:13:36 +0900

    os 수정

--- app.py
+++ app.py
@@ -154,4 +154,6 @@ def reset_all():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
